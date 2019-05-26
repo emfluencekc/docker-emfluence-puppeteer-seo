64d97aea0e6b97b8763d295a0d30418f332c7d25
Tristan Payne <tpayne@emfluence.com>
2019-05-26 16:45:54 -0500

    Fixes for chromium inside docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,24 @@
 FROM node:11.15-slim
 
+# Install latest chrome dev package, which installs the necessary libs to
+# make the bundled version of Chromium that Puppeteer installs work.
+RUN  apt-get update \
+    && apt-get install -y wget --no-install-recommends \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-unstable --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
 COPY index.js package.json package-lock.json /
 
-RUN npm install
+RUN npm install \
+    && groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
+    && mkdir -p /home/pptruser/Downloads \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser /node_modules
+
+# Run everything after as non-privileged user.
+USER pptruser
 
 CMD npm run start
--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 
 const app = express();
 
 app.get('/', async (req, res) => {
     try {
-        const browser = await puppeteer.launch({ headless: true });
+        console.log('ohi');
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: '/usr/bin/google-chrome-unstable'
+        });
+        console.log('ohi2');
         const page = await browser.newPage();
+        console.log('ohi3');
         await page.setUserAgent("emfluence-puppeteer-seo");
+        console.log('ohi4');
         await page.goto(req.query.url, { waitUntil: 'networkidle0' });
+        console.log('ohi5');
         const html = await page.content();
+        console.log('ohi6');
         await browser.close();
+        console.log('ohi7');
 
         return res.status(200).send(html);
     }
     catch (error) {
+        console.log(error);
         return res.status(500).send();
     }
 });
