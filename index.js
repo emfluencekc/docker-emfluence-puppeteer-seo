01326464aaf948585824c0770455d55ce8680fe4
Tristan Payne <tpayne@emfluence.com>
2019-06-04 14:23:55 -0500

    Remove debug

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ const app = express();
 
 app.get('/', async (req, res) => {
     try {
+        console.log('-----Start Request-----');
         console.log(req.query.url);
-        console.log('Open browser');
         const browser = await puppeteer.connect({ browserWSEndpoint: 'ws://chrome:3000' });
-        console.log('New page');
         const page = await browser.newPage();
         await page.setUserAgent("emfluence-puppeteer-seo");
-        console.log('Check for auth', process.env);
         if (process.env.auth_user && process.env.auth_pass) {
             await page.authenticate({username: process.env.auth_user, password: process.env.auth_pass});
         }
-        console.log('Goto page');
         await page.goto(req.query.url, { waitUntil: 'networkidle0' });
         const html = await page.content();
-        console.log('Close browser');
         await browser.close();
 
+        console.log('-----End Request-----');
         return res.status(200).send(html);
     }
     catch (error) {
